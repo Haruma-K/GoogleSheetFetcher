aeceb9181c7341fb46b6cd361150dd44f45945e9
Haruki Yano <haruki.y.11g@gmail.com>
2020-05-06 21:39:25 +0900

    Settings for Rider

---
+++ Assets/Workspace/Editor/CSProjectPostProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class CSProjectPostProcessor : AssetPostprocessor
+{
+    private static string OnGeneratedCSProject(string path, string content)
+    {
+        var document = XDocument.Parse(content);
+        var folderPath = Application.dataPath
+            .Replace("Assets", "")
+            .Replace('/', Path.DirectorySeparatorChar);
+
+        foreach (var descendant in document.Root.Descendants())
+        {
+            if (descendant.Name.LocalName != "Compile")
+                continue;
+
+            var attribute = descendant.Attribute("Include");
+            if (attribute == null)
+                continue;
+            if (!attribute.Value.Contains(".cs") || !attribute.Value.Contains(folderPath))
+                continue;
+
+            attribute.Value = attribute.Value.Replace(folderPath, "");
+        }
+        
+        return document.Declaration + Environment.NewLine + document.Root;
+    }
+}
